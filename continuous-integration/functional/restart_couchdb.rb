4e9f810f3863e718ae9790252a4b4c0467b2d106
Tim Hinderliter <tim@opscode.com>
2010-02-05 11:23:06 -0800

    err, forgot to include 'cmdutil' for scripting functinoality

--- continuous-integration/functional/restart_couchdb.rb
+++ continuous-integration/functional/restart_couchdb.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+class << @self
+  require "#{File.dirname(__FILE__)}/cmdutil"
+end
+
 #timmbp-oc:~/devel/opscode tim$ ps uxaw|grep /couchdb/|grep -v grep|grep beam
 #root     43591  25.3  0.6  2491568  23308 s002  R+    9:09PM   7:52.69 /usr/local/lib/erlang/erts-5.7.4/bin/beam.smp -Bd -K true -- -root /usr/local/lib/erlang -progname erl -- -home /Users/tim -- -noshell -noinput -smp auto -sasl errlog_type error -pa /usr/local/apache-couchdb-0.10.0/lib/couchdb/erlang/lib/couch-0.10.0/ebin /usr/local/apache-couchdb-0.10.0/lib/couchdb/erlang/lib/mochiweb-r97/ebin /usr/local/apache-couchdb-0.10.0/lib/couchdb/erlang/lib/ibrowse-1.5.2/ebin /usr/local/apache-couchdb-0.10.0/lib/couchdb/erlang/lib/erlang-oauth/ebin -eval application:load(ibrowse) -eval application:load(oauth) -eval application:load(crypto) -eval application:load(couch) -eval crypto:start() -eval ssl:start() -eval ibrowse:start() -eval couch_server:start([ "/usr/local/apache-couchdb-0.10.0/etc/couchdb/default.ini", "/usr/local/apache-couchdb-0.10.0/etc/couchdb/local.ini"]), receive done -> done end.
 
@@ -18,9 +22,7 @@ end
 if pid
   cmd = "sudo kill -TERM #{pid}"
   puts "Killing couchdb process at #{pid} with SIGTERM"
-  puts "CMD: #{cmd}"
-  res = system cmd
-  puts ".. kill returned #{res}"
+  run cmd
 else
   puts "No couchdb process found, skipping.."
 end
