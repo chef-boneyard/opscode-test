3661f5e27e19f5963b3e7bc9fa9ef89d6494c167
Christopher Brown <cb@opscode.com>
2010-02-18 16:47:49 -0800

    pretty up repo-status.rb, add git-wtf

--- repo-status.rb
+++ repo-status.rb
@@ -2,6 +2,8 @@
 
 %w{opscode-chef opscode-authz mixlib-authorization mixlib-authentication opscode-account mixlib-cli mixlib-log mixlib-config chef opscode-rest couchrest rest-client}.each do |proj|
   Dir.chdir(proj) do
-    system("echo $(pwd) && git branch && git status")
+    puts "-----------------------------------"
+    system("echo $(pwd); git branch; git status; ruby ../opscode-test/git-wtf")
+    puts "-----------------------------------"
   end
 end
