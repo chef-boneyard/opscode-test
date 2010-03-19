30969a4609e5e8b18f4de3178157a112caa76faf
Daniel DeLeo <dan@opscode.com>
2010-03-19 15:30:49 -0700

    opscode cucumber graduated to its own project

--- opscode_cucumber.rb
+++
@@ -1,5 +0,0 @@
-$:.unshift File.dirname(__FILE__)
-require "opscode_cucumber/opscode_world"
-require "opscode_cucumber/couchdb"
-require "opscode_cucumber/rest"
-require "opscode_cucumber/tempfiles"
