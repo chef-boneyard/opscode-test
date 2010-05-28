593db223684e2774889759eded65af07d87fab8a
Christopher Brown <cb@opscode.com>
2010-05-28 13:38:29 -0700

    added mixlib-localization to the 'git pull everything' script

--- repo-status.rb
+++ repo-status.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-%w{chef couchrest mixlib-authentication mixlib-authorization mixlib-cli mixlib-config mixlib-log nginx-sysoev opscode-account opscode-audit opscode-authz opscode-authz-internal opscode-cert-erlang opscode-chef opscode-rest opscode-test rest-client}.each do |proj|
+%w{chef couchrest mixlib-authentication mixlib-authorization mixlib-localization mixlib-cli mixlib-config mixlib-log nginx-sysoev opscode-account opscode-audit opscode-authz opscode-authz-internal opscode-cert-erlang opscode-chef opscode-rest opscode-test rest-client}.each do |proj|
   Dir.chdir(proj) do
     puts "-----------------------------------"
-    system("echo $(pwd); git branch; git status; ruby ../opscode-test/git-wtf")
+    system("echo $(pwd); git branch; git pull; git status; ruby ../opscode-test/git-wtf")
     puts "-----------------------------------"
   end
 end
