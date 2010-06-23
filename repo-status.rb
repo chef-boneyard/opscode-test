2a1fcd0988c3e8f546f6bea8148e27f9c1d330ba
Christopher Brown <cb@opscode.com>
2010-06-23 11:51:49 -0700

    pulling extra repos

--- repo-status.rb
+++ repo-status.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-%w{chef couchrest mixlib-authentication mixlib-authorization mixlib-localization mixlib-cli mixlib-config mixlib-log nginx-sysoev opscode-account opscode-audit opscode-authz opscode-authz-internal opscode-cert-erlang opscode-chef opscode-rest opscode-test rest-client}.each do |proj|
+%w{opscode-cucumber chef couchrest mixlib-authentication mixlib-authorization mixlib-localization mixlib-cli mixlib-config mixlib-log nginx-sysoev opscode-account opscode-audit opscode-authz opscode-authz-internal opscode-cert-erlang opscode-chef opscode-rest opscode-test rest-client opscode-org-creator}.each do |proj|
   Dir.chdir(proj) do
     puts "-----------------------------------"
     system("echo $(pwd); git branch; git pull; git status; ruby ../opscode-test/git-wtf")
