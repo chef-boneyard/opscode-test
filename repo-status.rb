5c3aeb6f3b8ef2faf1cc472a24a2d86d520bd022
Christopher Brown <cb@opscode.com>
2010-08-04 10:19:56 -0700

    added opscode-corpsite to repo list

--- repo-status.rb
+++ repo-status.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-%w{opscode-corpsite opscode-cucumber chef couchrest mixlib-authentication mixlib-authorization mixlib-localization mixlib-cli mixlib-config mixlib-log nginx-sysoev opscode-account opscode-audit opscode-authz opscode-authz-internal opscode-cert-erlang opscode-chef opscode-rest opscode-test rest-client opscode-org-creator}.each do |proj|
+%w{opscode-corpsite opscode-cucumber ohai chef couchrest mixlib-authentication mixlib-authorization mixlib-localization mixlib-cli mixlib-config mixlib-log nginx-sysoev opscode-account opscode-audit opscode-authz opscode-authz-internal opscode-cert-erlang opscode-chef opscode-rest opscode-test rest-client opscode-org-creator}.each do |proj|
   Dir.chdir(proj) do
     puts "-----------------------------------"
     system("echo $(pwd); git branch; git pull; git status; ruby ../opscode-test/git-wtf")
