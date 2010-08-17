05776e637fa3addf87bf978d396806a54b93566e
Tim Hinderliter <tim@opscode.com>
2010-08-16 17:16:16 -0700

    run-cucumber script for hudson

---
+++ continuous-integration/hudson/run-cucumber.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# - First argument is path within /srv to run, i.e. the project.
+#   All other arguments are passed to cucumber.
+
+STARTING_PWD=$PWD
+
+export GEM_HOME=/srv/localgems
+export GEM_PATH=/srv/localgems
+export PATH=/srv/localgems/bin:$PATH
+
+cd /srv/"$1"/current
+shift
+
+cucumber "$@" --format junit --out $STARTING_PWD/junit_output
+
