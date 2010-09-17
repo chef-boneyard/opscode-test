c8eca219751f8e153c648c0dff554671d834d771
Tim Hinderliter <tim@opscode.com>
2010-09-17 14:29:48 -0700

    add run-rake script for hudson slave

---
+++ continuous-integration/hudson/run-rake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# - First argument is path within /srv to run, i.e. the project.
+#   All other arguments are passed to cucumber.
+
+STARTING_PWD="$PWD"
+
+export GEM_HOME=/srv/localgems
+export GEM_PATH=/srv/localgems
+export PATH=/srv/localgems/bin:$PATH
+
+cd /srv/"$1"/current
+shift
+
+rake -f /srv/localgems/gems/ci_reporter-1.6.2/stub.rake ci:setup:rspec "$@"
