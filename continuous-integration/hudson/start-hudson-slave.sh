62db1cd35a8d6c8966aeb5f79e3f901ba837bfc5
Tim Hinderliter <tim@opscode.com>
2010-09-16 17:30:03 -0700

    sudo before running slave, doh

--- continuous-integration/hudson/start-hudson-slave.sh
+++ continuous-integration/hudson/start-hudson-slave.sh
@@ -5,7 +5,7 @@ OLD_PWD=$PWD
 sudo hostname ubuntu-ci-slave-euca
 
 # run chef with the system GEM's.
-sudo chef-client -l debug
+#sudo chef-client -l debug # this is run by the hudson builder.
 
 # Everything we do from now on uses the /srv-installed GEM's, bins, etc.
 export GEM_HOME=/srv/localgems GEM_PATH=/srv/localgems PATH=/srv/localgems/bin:$PATH
