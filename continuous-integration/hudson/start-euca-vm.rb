008b5a45c3269c7b943790c6d79fe93ab72eded0
Tim Hinderliter <tim@opscode.com>
2010-09-16 17:00:32 -0700

    setting path/etc before running setup:test

--- continuous-integration/hudson/start-euca-vm.rb
+++ continuous-integration/hudson/start-euca-vm.rb
@@ -261,7 +261,7 @@ else
 end
 
 if wait_for_ssh_running(instance, use_private_ip)
-  do_on_remote(instance, use_private_ip, "'sudo hostname ubuntu-ci-slave-euca && sudo chef-client -l debug && (cd /srv/opscode-test/current && sudo env GEM_HOME=/srv/localgems GEM_PATH=/srv/localgems PATH=/srv/localgems:$PATH rake setup:test) && sudo env GEM_HOME=/srv/localgems GEM_PATH=/srv/localgems PATH=/srv/localgems:$PATH java -jar slave.jar'")
+  do_on_remote(instance, use_private_ip, "'sudo hostname ubuntu-ci-slave-euca && sudo chef-client -l debug && (cd /srv/opscode-test/current && sudo env GEM_HOME=/srv/localgems GEM_PATH=/srv/localgems PATH=/srv/localgems/bin:$PATH rake setup:test) && sudo env GEM_HOME=/srv/localgems GEM_PATH=/srv/localgems PATH=/srv/localgems/bin:$PATH java -jar slave.jar'")
 else
   puts "ERROR: Not running slave JAR as ssh never responded"
 end
