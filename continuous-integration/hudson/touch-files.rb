c020be8422c10216c6ad3b0adf0fd38e6825c861
Tim Hinderliter <tim@opscode.com>
2010-09-20 11:13:02 -0700

    Ruby's touch (through File.utime) doesn't show the NFS timestamp delta behavior; use this instead of coreutils 'touch'

---
+++ continuous-integration/hudson/touch-files.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+# Compensate for the timestamp delta between an NFS server hosting files 
+# and the local time. 
+#
+# This is required as our NFS server isn't running NTP update, and is sometimes 
+# many seconds off of the Hudson VM and the slave builder VM. If all the 
+# machines are running an NTP updater then this script isn't required.
+#
+# The root issue is that Hudson won't accept JUnit XML output files if they are 
+# over a certain number of seconds old.
+#
+# For some reason, Linux 'touch' shows this behavior, but Ruby's touch (through 
+# "File.utime") updates with the correct system time (as opposed to the NFS 
+# server time). So, we're just going to update each of the input files to the
+# current time.
+
+now = Time.now
+ARGV.each do |filename|
+  File.utime(now, now, filename)
+end
+
+  
+
+  
+
+  
+  
