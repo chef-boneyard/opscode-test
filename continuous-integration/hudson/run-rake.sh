8d6180aff0d4afbb424b15da4917c9c54f44f904
Tim Hinderliter <tim@opscode.com>
2010-09-20 14:26:40 -0700

    use ci_reporter environment variables instead of janky mv operation to tell it where to stash JUnit XML output; hopefully turn out stdout output as well as JUNIT XML (through env variable)

--- continuous-integration/hudson/run-rake.sh
+++ continuous-integration/hudson/run-rake.sh
@@ -12,13 +12,17 @@ export PATH=/srv/localgems/bin:$PATH
 cd /srv/"$1"/current
 shift
 
-rake -f /srv/localgems/gems/ci_reporter-1.6.2/stub.rake ci:setup:rspec "$@"
-RESULT=$?
-
+# make sure the output directory exists..
 if [ ! -d "$STARTING_PWD/spec_reports" ]; then
     mkdir "$STARTING_PWD"/spec_reports
 fi
-mv -v spec/reports/* "$STARTING_PWD"/spec_reports/
+# Then tell CI_REPORTER where to stash its files.
+export CI_CAPTURE=on
+export CI_REPORTS="$STARTING_PWD"/spec_reports
+
+rake -f /srv/localgems/gems/ci_reporter-1.6.2/stub.rake ci:setup:rspec "$@"
+RESULT=$?
+
 ruby /srv/opscode-test/current/continuous-integration/hudson/touch-files.rb "$STARTING_PWD/spec_reports/"
 
 # return with the same error code that rake returned.
