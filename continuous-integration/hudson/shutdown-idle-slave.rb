d620abb8992c3bfb795e3954984269e31599f3c1
Tim Hinderliter <tim@opscode.com>
2010-09-21 16:42:19 -0700

    script to halt computer if slave jar isn't running

---
+++ continuous-integration/hudson/shutdown-idle-slave.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+# Shuts the system down (via halt) if it's been up for at least 30 minutes and
+# there is no 'java slave.jar' process running.
+
+unless File.exists?("/proc/uptime")
+  raise "/proc/uptime doesn't exist! This only works on Linux."
+end
+
+uptime = 0
+File.open("/proc/uptime", "r") do |uptime_file|
+  uptime_file.each_line do |line|
+    if line =~ /([\d\.]+) ([\d\.]+)/
+      uptime = $1
+    end
+  end
+end
+
+slave_jar_running = false
+IO.popen("ps uaxwwww | grep java | grep slave | grep -v grep", "r") do |ps_proc|
+  ps_proc.each_line do |line|
+    if line =~ /java -jar slave\.jar/
+      slave_jar_running = true
+    end
+  end
+end
+
+#puts "slave_jar_running: #{slave_jar_running}"
+#puts "uptime: #{uptime}"
+
+if !slave_jar_running && uptime > 30
+  system 'halt'
+end
