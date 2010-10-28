c3dab67cf68c2377109b139ba4df7a945d034a02
Daniel DeLeo <dan@opscode.com>
2010-10-27 17:27:46 -0700

    update opscode-chef recipe to do some bundling

--- continuous-integration/environments/opscode-ci/cookbooks/opscode-chef/recipes/library.rb
+++ continuous-integration/environments/opscode-ci/cookbooks/opscode-chef/recipes/library.rb
@@ -6,6 +6,8 @@
 # Copyright 2009, Opscode, Inc.
 #
 
+gem_package "bundler"
+
 include_recipe "opscode-base"
 
 env = node["environment"]
@@ -18,17 +20,33 @@ directory app['deploy_to'] do
   recursive true
 end
 
+directory "/srv/opscode-chef/shared/vendor" do
+  mode "0755"
+  owner "opscode"
+  group "opscode"
+end
+
 deploy_revision app['id'] do
   #action :force_deploy
   revision env['opscode-chef-revision'] || env['default-revision']
   repository 'git@github.com:' + (env['opscode-chef-remote'] || env['default-remote']) + '/opscode-chef.git'
   remote (env['opscode-chef-remote'] || env['default-remote'])
   ##restart_command "if test -L /etc/init.d/opscode-chef; then sudo /etc/init.d/opscode-chef restart; fi"
+  symlinks("system" => "public/system", "pids" => "tmp/pids", "log" => "log", "vendor" => "vendor")
   symlink_before_migrate Hash.new
-  user app['owner']
-  group app['group']
+  #user app['owner']
+  #group app['group']
   deploy_to app['deploy_to']
   migrate false
+
+  before_restart do
+    execute("bundle install --deployment") do
+      cwd("/srv/opscode-chef/current")
+      user(app['owner'])
+      group(app['group'])
+    end
+  end
+
 end
 
 directory "/srv/opscode-chef/shared/cookbooks_cache" do
