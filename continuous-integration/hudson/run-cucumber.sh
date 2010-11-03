d707300935e70f81904eaf67060d91faa3682419
Tim Hinderliter <tim@opscode.com>
2010-11-03 11:16:17 -0700

    Remove the restart of CouchDB from each cucumber run, as this problem has gone away based on the replications changes; remove junit output files before cukes are run, as if tests have been removed between the last cuke run and this one, the old stale files will still be sitting around.

--- continuous-integration/hudson/run-cucumber.sh
+++ continuous-integration/hudson/run-cucumber.sh
@@ -9,12 +9,11 @@ export GEM_HOME=/srv/localgems
 export GEM_PATH=/srv/localgems
 export PATH=/srv/localgems/bin:$PATH
 
-# restart couchdb to (hopefully) mitigate the replication issues (e.g., opscode-account
-# does many replicates and eventually causes Couch to start misbehaving)
-/etc/init.d/couchdb restart
-
 cd /srv/"$1"/current
 shift
+
+# Remove all the junit output files first so no stale results are around.
+rm -f "$STARTING_PWD/junit_output/"*
 
 if [ -f Gemfile.lock ]; then
   bundle exec cucumber "$@" --format junit --out "$STARTING_PWD/junit_output"
@@ -24,6 +23,9 @@ fi
 
 RESULT=$?
 
+# Touch the mtime of the JUnit output files to work around the fact that the
+# NFS server current time may be different than the slave current time, which if
+# the times are different enough, causes Hudson to ignore all the JUnit output.
 ruby /srv/opscode-test/current/continuous-integration/hudson/touch-files.rb "$STARTING_PWD/junit_output/"
 
 # return with the same error code that cucumber returned.
