fe619a4f40a23bb40376f729f7ea0becf3dcf006
Seth Chisamore <schisamo@opscode.com>
2013-05-29 17:24:34 -0400

    rename authz_* config values to bifrost_*

--- lib/opscode/test/config.rb
+++ lib/opscode/test/config.rb
@@ -18,7 +18,7 @@ module Opscode::Test
     account_db = CouchRest::Database.new(cdb_server, 'opscode_account')
 
     Mixlib::Authorization::Config.default_database          = account_db
-    Mixlib::Authorization::Config.authorization_service_uri = "http://#{config.authz_host}:#{config.authz_port}"
+    Mixlib::Authorization::Config.authorization_service_uri = "http://#{config.bifrost_host}:#{config.bifrost_port}"
     require 'mixlib/authorization/models'
   end
 
@@ -48,8 +48,8 @@ module Opscode::Test
     attr_accessor :couchdbauthz_port
 
     # config for authz
-    attr_accessor :authz_host
-    attr_accessor :authz_port
+    attr_accessor :bifrost_host
+    attr_accessor :bifrost_port
 
     attr_accessor :superuser_cert
     attr_accessor :superuser_key
@@ -69,8 +69,8 @@ module Opscode::Test
   couchdbauthz_host: #{couchdbauthz_host}
   couchdbauthz_port: #{couchdbauthz_port}
 
-  authz_host:        #{authz_host}
-  authz_port:        #{authz_port}
+  bifrost_host:        #{bifrost_host}
+  bifrost_port:        #{bifrost_port}
 
   superuser_cert:    #{superuser_cert}
   superuser_key:     #{superuser_key}
